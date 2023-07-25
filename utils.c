28a619da42d482711586343c9551b3992b343986
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-07-25 12:40:57 -0700

    updates

--- utils.c
+++ utils.c
@@ -77,10 +77,10 @@ long int convert_size_number(long int num, int size)
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
-	if (size == S_LONG)
-		return (num);
-	else if (size == S_SHORT)
-		return ((unsigned short)num);
-		return ((unsigned int)num);
+if (size == S_LONG)
+return (num);
+else if (size == S_SHORT)
+return ((unsigned short)num);
+return ((unsigned int)num);
 }
 
