08d413ce1528a0d85fc85837fcb31632946f3541
samtos007 <tobibamigbademobax@gmail.com>
2023-07-25 21:56:34 -0700

    updated ver

--- _printf.c
+++ _printf.c
@@ -3,13 +3,16 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
+ * _printf - Printf functionn prototype
  * @format: format.
  * Return: Printed chars.
  */
+
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
+	int i;
+	int printed = 0;
+	int printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
