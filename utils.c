48af519f26bff84f22f2354fd7c3cdd93c231e0a
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-07-27 14:05:19 -0700

    updates

--- utils.c
+++ utils.c
@@ -72,7 +72,6 @@ long int convert_size_number(long int num, int size)
  * convert_size_unsgnd - Casts a number to the specified size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
- *
  * Return: Casted value of num
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
@@ -81,6 +80,7 @@ long int convert_size_unsgnd(unsigned long int num, int size)
 		return (num);
 	else if (size == S_SHORT)
 		return ((unsigned short)num);
-		return ((unsigned int)num);
+
+	return ((unsigned int)num);
 }
 
